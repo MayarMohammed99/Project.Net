2b297b851bb16c2e099e40525a48c558cb63514e
mayarawad99 <mayarawad77@gmail.com>
2023-09-17 15:09:29 +0300

    Added my project

--- source/repos/firstday/firstday/Class1.cs
+++ source/repos/firstday/firstday/Class1.cs
@@ -2,7 +2,9 @@
 {
     public class Class1
     {
-     
+        public int id;
+        public string username;
+        System.Console.WriteLine("Nee info");
         public void display()
         {
            System.Console.WriteLine("display info");
